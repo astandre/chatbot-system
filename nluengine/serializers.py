1dd0c9d02c4f476d1f928cd21ed353c865c0531b
Andre <andreherrera97@hotmail.com>
2018-09-26 22:45:37 -0500

    Keeping context to answer intents

--- nluengine/serializers.py
+++ nluengine/serializers.py
@@ -9,3 +9,13 @@ class QuestionSerializer(serializers.Serializer):
         Create and return a new `Snippet` instance, given the validated data.
         """
         return validated_data
+
+
+class IntentSerializer(serializers.Serializer):
+    intent = serializers.CharField(required=True, max_length=30)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return validated_data
